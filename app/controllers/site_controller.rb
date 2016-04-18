39b35e72db97e19538e2f877e9d3f101bec2c408
Andreza Feu <andrezafeu@gmail.com>
2016-04-18 11:14:46 -0400

    Created contact page that displays email address

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,7 @@ class SiteController < ApplicationController
 	def home
 		render 'home' #name of view templates
 	end
+	def contact
+		render 'contact'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   get '/', to: 'site#home'
+
+  get '/contact', to: 'site#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
