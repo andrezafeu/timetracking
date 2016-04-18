82fb652c466985af1576ba029da311d3dedae399
Andreza Feu <andrezafeu@gmail.com>
2016-04-18 12:04:06 -0400

    Created page say_name, that receives the name from the user as a parameter and displays it on the say_name page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'site#contact'
 
+  get '/say_name/:name', to: 'site#say_name'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
