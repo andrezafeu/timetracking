73378996327795d33981c565c8dc8fb67508c5f2
Andreza Feu <andrezafeu@gmail.com>
2016-04-18 11:54:46 -0400

    Added a partial page to be used for user registration in the future

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,7 @@
 class SiteController < ApplicationController
 	def home
-		render 'home' #name of view templates
+		render 'home' #name of views template. if the name of views template is the same of 
+					  #the method, you don't need this line
 	end
 	def contact
 		render 'contact'
