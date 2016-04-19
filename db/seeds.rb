150a017c8e44e7524a5afe56b4b2712be8ad55d4
Andreza Feu <andrezafeu@gmail.com>
2016-04-19 14:04:49 -0400

    Delete projects created on terminal and pass projects through seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Project.create(name: 'Ironhack', description: 'Ironhack is a project.')
+Project.create(name: 'Timetracking', description: 'Tracks the time spend on projects')
+Project.create(name: 'Recipes', description: 'Track my favorite recipes')
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
