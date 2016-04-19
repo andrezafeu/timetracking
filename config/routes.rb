307c1961e9f80383bd004ad2c2a8f6f8cf73b828
Andreza Feu <andrezafeu@gmail.com>
2016-04-19 12:20:50 -0400

    Set up projects controller and erb view page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'site#home'
 
+  get '/projects', to: 'projects#index'
+
   get '/contact', to: 'site#contact'
 
   get '/say_name/:name', to: 'site#say_name'
