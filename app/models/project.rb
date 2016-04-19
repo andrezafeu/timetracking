5653671c12190af24464d20d345b635a8e39217a
Andreza Feu <andrezafeu@gmail.com>
2016-04-19 11:24:49 -0400

    Create a new project (row) on projects table

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-	set_table_name 'projects' #this is not neccessairy, because rails will
+	# set_table_name 'projects' #this is not neccessairy, because rails will
 							#automatically create a table with this name
 end
