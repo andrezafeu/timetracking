88fe18a48caf5b926719fc91ec77fa999aaa197e
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 16:56:27 -0400

    Validate if project name has only alphanumeric characters and spaces

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,8 @@ class Project < ActiveRecord::Base
 	# set_table_name 'projects' #this is not neccessairy, because rails will
 							#automatically create a table with this name
 	has_many :time_entries
-	validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
+	validates :name, presence: true, 
+	uniqueness: true, 
+	length: { maximum: 30 }, 
+	format: { with: /\s[a-zA-Z0-9]/}
 end
