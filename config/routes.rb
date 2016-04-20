c043b366a1920c989af8a0f03253be8f9d348bd7
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 15:17:36 -0400

    Create route for new time_entry

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get '/projects/:project_id/time_entries', to: 'time_entries#index'
 
+  get '/projects/:project_id/time_entries/new'
+
   get '/contact', to: 'site#contact'
 
   get '/say_name/:name', to: 'site#say_name'
