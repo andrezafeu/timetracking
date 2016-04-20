b848beefd18aadcd1b1397826484ad841cc8e4e3
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 16:12:14 -0400

    Add page time_entries/new

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,6 +1,14 @@
 class TimeEntriesController < ApplicationController
 	def index
 		@my_project = Project.find(params[:project_id])
+		@my_entries = @my_project.time_entries
+	end
+	def new
+		@my_project = Project.find_by(id: params[:project_id])
 		@my_entry = @my_project.time_entries.new
+		render "new"
+	end
+	def create
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
 
   get '/projects/:project_id/time_entries', to: 'time_entries#index'
 
-  get '/projects/:project_id/time_entries/new'
+  get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
+
+  post '/projects/:project_id/time_entries', 
+    to: 'time_entries#create', as: :project_time_entries
 
   get '/contact', to: 'site#contact'
 
