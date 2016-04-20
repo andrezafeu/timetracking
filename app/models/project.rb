b4c5979da42349f42e4525843406f82bed024aba
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 12:15:19 -0400

    Create time_entries for projects on seed file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
 	# set_table_name 'projects' #this is not neccessairy, because rails will
 							#automatically create a table with this name
+	has_many :time_entries
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-Project.create(name: 'Ironhack', description: 'Ironhack is a project.')
-Project.create(name: 'Timetracking', description: 'Tracks the time spend on projects')
-Project.create(name: 'Recipes', description: 'Track my favorite recipes')
+project1 = Project.create(name: 'Ironhack', description: 'Ironhack is a project.')
+project2 = Project.create(name: 'Timetracking', description: 'Tracks the time spend on projects')
+project3 = Project.create(name: 'Recipes', description: 'Track my favorite recipes')
 
+project1.time_entries.create(hours: 1, minutes: 45)
+project2.time_entries.create(hours: 0, minutes: 30)
+project3.time_entries.create(hours: 2, minutes: 0)
+
+# entry = project.time_entries.new
+# entry.minutes = 32
+# entry.hours = 1
+# entry.save
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
