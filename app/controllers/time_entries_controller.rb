580938050aed23a9fbc94bb10f929f683c9ac640
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 15:18:42 -0400

    Implement new action for time_entry in the controller

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,6 +1,6 @@
 class TimeEntriesController < ApplicationController
 	def index
 		@my_project = Project.find(params[:project_id])
-		@my_entries = @my_project.time_entries
+		@my_entry = @my_project.time_entries.new
 	end
 end
