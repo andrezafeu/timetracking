2bc608d4e778c0f088c6df32d43923494b13304e
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 14:45:02 -0400

    Add date to time_entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@ project1 = Project.create(name: 'Ironhack', description: 'Ironhack is a project.
 project2 = Project.create(name: 'Timetracking', description: 'Tracks the time spend on projects')
 project3 = Project.create(name: 'Recipes', description: 'Track my favorite recipes')
 
-project1.time_entries.create(hours: 1, minutes: 45)
-project1.time_entries.create(hours: 0, minutes: 30)
-project2.time_entries.create(hours: 0, minutes: 30)
-project3.time_entries.create(hours: 2, minutes: 0)
+project1.time_entries.create(hours: 1, minutes: 45, date: Time.new(2016,4,16,13,30))
+project1.time_entries.create(hours: 0, minutes: 30, date: Time.now)
+project2.time_entries.create(hours: 0, minutes: 30, date: Time.now)
+project3.time_entries.create(hours: 2, minutes: 0, date: Time.new(2016,4,17,13,0))
 
 # entry = project.time_entries.new
 # entry.minutes = 32
