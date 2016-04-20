6a00f1bc9ffdc7f7b93a99fe15bfae24af04465d
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 16:52:35 -0400

    Validate if project name is maximum 30 characters

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
 	# set_table_name 'projects' #this is not neccessairy, because rails will
 							#automatically create a table with this name
 	has_many :time_entries
-	validates :name, presence: true, uniqueness: true
+	validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
 end
