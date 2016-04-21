fd2c880275b7f724b40197252d32b0c620ae945f
Andreza Feu <andrezafeu@gmail.com>
2016-04-21 11:38:44 -0400

    Create action for edit project

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -22,4 +22,8 @@ class TimeEntriesController < ApplicationController
 			render "new"
 		end
 	end
+	def edit
+		@my_project = Project.find params[:project_id]
+		@my_entry = @@my_project.time_entries.find params[:id]
+	end
 end
