1ceaa35d4628c68b7fc7e1abc259c5f38279ac22
Andreza Feu <andrezafeu@gmail.com>
2016-04-21 15:37:20 -0400

    Restrict the routes created with resources routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'site#home'
 
-  resources :projects do
-    resources :time_entries
+  resources :projects, only: [:index, :show, :new, :create] do
+    resources :time_entries, except: [:show]
   end
 
   get '/contact', to: 'site#contact'
